26ee3a694e68212be91e3ebf7f6a9483d6e4c71a
Yotam Markman <yotam.markman@gmail.com>
2025-05-11 17:19:18 +0300

    Starting to build front end

---
+++ src/services/socket.js
@@ -0,0 +1,13 @@
+// src/services/socket.js
+import { io } from 'socket.io-client';
+
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000';
+
+const socket = io(SOCKET_URL, {
+  autoConnect: false,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
+
+export default socket;
